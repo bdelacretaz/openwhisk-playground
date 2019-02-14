6f687c6c3882ff1c7e66c8a6f726f41d7c55f388
Bertrand Delacretaz <bdelacretaz@apache.org>
2019-02-14 10:41:28 +0100

    README

--- iceland-cars/iceland-cars.js
+++ iceland-cars/iceland-cars.js
@@ -1,18 +1,4 @@
 // OpenWhisk minimal integration Web function example
-// Get a car's make from the Icelandic apis.is service
-// See https://github.com/apache/incubator-openwhisk/blob/master/docs/webactions.md
-//
-// Test as follows, on an OpenWhisk setup:
-//    $ wsk -i action create car car.js --web true
-//    ok: created action car
-//    $ export URL=$(wsk -i action get car --url | grep http)
-//    # Known good prefixes: aa02*, aa03*, aa12*
-//    $ export prefix=aa12
-//    $ for i in 1 2 3 4 5 6 7 8 9; do export carId=$prefix$i ; curl -k "$URL?carId=$carId" ; echo ; done
-//    According to https://apis.is/car the car having number AA121 is a FORD - LTD (Brúnn)
-//    According to https://apis.is/car the car having number AA122 is a LADA - 2105 (Hvítur)
-//    ...
-//
 var request = require('request')
 
 function main (params) {
