7558f42a71169f01a13372c512e57b9542420024
Bertrand Delacretaz <bdelacretaz@apache.org>
2019-04-05 15:54:21 +0200

    Make eslint happier

--- statebox/state-store.js
+++ statebox/state-store.js
@@ -1,71 +1,64 @@
-'use strict'
-
-const REDIS = require('redis')
+/* eslint-disable no-console */
+const REDIS = require('redis');
 const uuidv4 = require('uuid/v4');
-const PREFIX = "c_"
-const ID_KEY = "c_ID"
 
 class StateStore {
-    constructor (options = {}) {
-        this.options = options ? options : {}
-        if(!options.host) {
-            options.host = 'localhost'
-        }
-        if(!options.port) {
-            options.port = 6379
-        }
-        console.log(`Connecting to Redis: ${options.host}:${options.port}`)
-            
-        this.client = REDIS.createClient({
-            host: options.host,
-            port: options.port,
-            retry_strategy: function (options) {
-                if (options.total_retry_time > 5 * 1000) {
-                    throw 'Redis retry time exhausted'
-                }
-                if (options.attempt > 10) {
-                    throw 'Redis max attempts exhausted'
-                }
-                // reconnect after semi-random time
-                return Math.min(options.attempt * 100, 3000);
-                }
-            }            
-        )
+  constructor(inputOptions = {}) {
+    this.options = inputOptions;
+    if (!this.options.host) {
+      this.options.host = 'localhost';
     }
-    
-    nextKey() {
-        return uuidv4()
+    if (!this.options.port) {
+      this.options.port = 6379;
     }
-    
-    async close() {
-      this.client.quit()
-    }
+    console.log(`Connecting to Redis: ${this.options.host}:${this.options.port}`);
 
-    async put(data, expirationSeconds) {
-        console.log(`StateStore.put()`)
-        const client = this.client
-        const key = this.nextKey()
-        return new Promise(async function (resolve, reject) {
-            data._CONTINUATION = key
-            client.setex(key, expirationSeconds, JSON.stringify(data))
-            console.log(`Saved continuation ${key}, expires in ${expirationSeconds} seconds`)
-            resolve(key)
-        })
-    }
-    
-    async get(key, callback) {
-        console.log(`StateStore.get(${key})`)
-        const client = this.client
-        return new Promise(async function (resolve, reject) {
-            client.get(key, (err, data) => { 
-                if(err) {
-                    reject(err)
-                } else {
-                    resolve({key : key, data:JSON.parse(data)})
-                }
-            })
-        })
-    }
-} 
+    this.client = REDIS.createClient({
+      host: this.options.host,
+      port: this.options.port,
+      retry_strategy: (options) => {
+        if (options.total_retry_time > 5 * 1000) {
+          throw new Error('Redis retry time exhausted');
+        }
+        if (options.attempt > 10) {
+          throw new Error('Redis max attempts exhausted');
+        }
+        // reconnect after semi-random time
+        return Math.min(options.attempt * 100, 3000);
+      },
+    });
+  }
 
-module.exports = StateStore
+  async close() {
+    this.client.quit();
+  }
+
+  async put(inputData, expirationSeconds) {
+    console.log('StateStore.put()');
+    const { client } = this;
+    const key = uuidv4();
+    const data = inputData;
+    return new Promise(async (resolve) => {
+      data.CONTINUATION = key;
+      client.setex(key, expirationSeconds, JSON.stringify(data));
+      console.log(`Saved continuation ${key}, expires in ${expirationSeconds} seconds`);
+      resolve(key);
+    });
+  }
+
+  async get(key) {
+    console.log(`StateStore.get(${key})`);
+    const { client } = this;
+    return new Promise(async (resolve, reject) => {
+      client.get(key, (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve({ key, data: JSON.parse(data) });
+        }
+      })
+    })
+  }
+}
+
+module.exports = StateStore;
