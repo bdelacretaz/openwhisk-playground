04eb89f0e6b7ee7debb0eeea2648094f8cfe9747
Bertrand Delacretaz <bdelacretaz@apache.org>
2019-04-09 23:15:44 +0200

    demo

---
+++ slack-command/slack-eval.js
@@ -0,0 +1,40 @@
+// Minimal OpenWhisk Slack command that evaluates an expression using jexl
+
+const jexl = require('jexl');
+
+function main (params) {
+
+  console.log(`request params: ${JSON.stringify(params, null, 2)}`)
+  const expr = (params && params.text) ? params.text : "";
+    
+  return new Promise((resolve, reject) => {
+    const now = new Date();
+
+    const context = {
+      hour: now.getHours(),
+      minute : now.getMinutes(),
+      second: now.getSeconds(),
+      year: now.getFullYear(),
+      // for additional demo random: Math.random(),
+    }
+
+    jexl.eval(expr, context)
+    .then(res =>{
+      const output = `${expr} = *${res}*`;
+      console.log(output);
+      return resolve({ body: `${output}\n`, headers:{ 'Content-Type': 'text/plain'}});
+    })
+    .catch(e => {
+      console.log(e);
+      return resolve({ body:`*ERROR*: ${e}`})
+    })
+  });
+}
+
+if (require.main === module) {
+  main({
+    text: process.argv[2],
+  });
+}
+
+module.exports.main = main
