23718d9b1fb9b4c1223a9267b272c8dba48fba67
Bertrand Delacretaz <bdelacretaz@apache.org>
2019-04-10 11:59:30 +0200

    Cleaner async main

--- slack-command/slack-eval.js
+++ slack-command/slack-eval.js
@@ -2,35 +2,44 @@
 
 const jexl = require('jexl');
 
-function main (params) {
+function plainText(content) {
+  return { body: `${content}\n`, headers:{ 'Content-Type': 'text/plain'}};
+}
+
+const now = new Date();
+
+const context = {
+  now : now,
+  hour: now.getHours(),
+  minute : now.getMinutes(),
+  second: now.getSeconds(),
+  year: now.getFullYear(),
+  space : " \n".repeat(10),
+  // for additional demo random: Math.random(),
+}
+
+// The actual OpenWhisk action
+async function main (params) {
 
   console.log(`request params: ${JSON.stringify(params, null, 2)}`)
   const expr = (params && params.text) ? params.text : "";
     
-  return new Promise((resolve, reject) => {
-    const now = new Date();
-
-    const context = {
-      hour: now.getHours(),
-      minute : now.getMinutes(),
-      second: now.getSeconds(),
-      year: now.getFullYear(),
-      // for additional demo random: Math.random(),
+    try {
+      return jexl.eval(expr, context)
+      .then(res =>{
+        const output = `${expr} = *${res}*`;
+        console.log(output);
+        return plainText(output);
+      })
+      .catch(e => {
+        return plainText(`*ERROR*: ${e}`);
+      })
+    } catch(e) {
+      return plainText(`*ERROR*: ${e}`);
     }
-
-    jexl.eval(expr, context)
-    .then(res =>{
-      const output = `${expr} = *${res}*`;
-      console.log(output);
-      return resolve({ body: `${output}\n`, headers:{ 'Content-Type': 'text/plain'}});
-    })
-    .catch(e => {
-      console.log(e);
-      return resolve({ body:`*ERROR*: ${e}`})
-    })
-  });
 }
 
+// Glue to run from the command line + OpenWhisk
 if (require.main === module) {
   main({
     text: process.argv[2],
