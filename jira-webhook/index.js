8e4245f999316d5fa516bf6102948ce8d13f6ba9
Bertrand Delacretaz <bdelacretaz@apache.org>
2019-04-16 12:33:50 +0200

    Basic JIRA client setup

--- jira-webhook/index.js
+++ jira-webhook/index.js
@@ -1,22 +1,46 @@
-JiraApi = require('jira').JiraApi;
+// Using https://github.com/floralvikings/jira-connector
+// See its README for how to generate jsdoc for that
+const JiraClient = require('jira-connector');
 
-const commentOnJira = (config, issue) => {
-  console.log(`Using JIRA host at ${config.host}:${config.port}, username ${config.username}`)
-  var jira = new JiraApi('https', config.host, config.port, config.username, config.password, '2', true);
-  jira.findIssue(issue.key, (error, issue) => {
-    if(error) {
-      throw new Error(error)
+const getJiraClient = (config) => {
+  return new JiraClient( {
+    host: config.host,
+    version: 2,
+    basic_auth: {
+        username: config.username,
+        password: config.password
     }
-    console.log('Issue status: ' + issue.fields.status.name);
-  });
-  return `Commenting on ${issue.key} at ${config.host}:${config.port}`
+});
+}
+const talkToJira = async (config, issue) => {
+  console.log(`Using JIRA host ${config.host}, username ${config.username}`)
+  if(!config.password) {
+    throw new Error("Using basic auth for now, password must be provided");
+  }
+  const jira = getJiraClient(config)
+
+  try {
+    // await jira.auth.login();
+    const data = await jira.issue.getIssue({ issueId: issue});
+    console.log(`Got issue ${issue}, ${data.self}, project=${data.fields.project.name}`);
+  } catch(e) {
+    console.log(`API ERROR ${e}`);
+  } 
+  /*
+  await jira.issue.addComment({
+    issueId : issue,
+    comment: `{ This is from the bot on ${issue}}`
+  })
+  .catch(e => { throw new Error(e) })
+  */
+  return `Talking to JIRA, ${issue.key} at ${config.host}`
 }
 
 const main = async (params) => {
   console.log(JSON.stringify(params, null, 2))
   const issue = params.issue
   if(issue) {
-    return { body: commentOnJira(params.jira, issue) }
+    return { body: talkToJira(params.jira, issue) }
   } else {
     const msg = "No issue key provided, doing nothing" 
     return { body: msg }
@@ -28,10 +52,9 @@ if (require.main === module) {
   main({
       issue: process.argv[2],
       jira: {
-        host: "b2019.atlassian.net",
-        port: 443,
-        username: process.argv[3],
-        password: process.argv[4]
+        host: process.argv[3],
+        username: process.argv[4],
+        password: process.argv[5]
       }
     });
 }
