52f542d89d0cbe4bb61c1a42698e1f2e8ec61af8
Bertrand Delacretaz <bdelacretaz@apache.org>
2022-04-13 10:45:24 +0200

    iCal feed test

---
+++ ical-feed/README.md
@@ -0,0 +1,27 @@
+ical-feed
+============
+
+Generate an iCal feed as an OpenWhisk serverless Action
+
+To test on the command-line:
+
+    npm install
+    node -e "console.log(require('./ical-feed.js').main())"
+
+To deploy to OpenWhisk, after setting up the `wsk` command
+for your environment:
+
+    $ npm install
+    $ export ACTION=ical-feed
+    $ zip -r action.zip package.json node_modules $ACTION.js && wsk action update $ACTION action.zip --web true --kind nodejs:10
+    ok: created action ical-feed
+    
+    $ export URL=$(wsk -i action get $ACTION --url | grep http)
+
+    $ open $URL
+
+To get info about any errors, use:
+
+    $ wsk -i activation get --last
+
+	
---
+++ ical-feed/ical-feed.js
@@ -0,0 +1,51 @@
+// Based on the https://www.npmjs.com/package/ics example
+// Validate at https://icalendar.org/validator.html
+// Display at 
+const ics = require('ics')
+
+const getEvents = () => {
+  const year = new Date().getFullYear();
+  const month = new Date().getMonth() + 1;
+  const days = [1, 12, 27, new Date().getDate()];
+  const events = [];
+  
+  for (let day of days) {
+    events.push({
+      start: [year, month, day, 6, 42],
+      duration: { hours: 1, minutes: 42 },
+      title: `Test for ${year}-${month}-${day}`,
+      description: `Test event for ${year}-${month}-${day}`,
+      location: 'Somewhere in Colorado',
+      url: 'http://grep.codeconsult.ch/',
+      geo: { lat: 40.0095, lon: 105.2669 },
+      categories: ['iCal', 'Testing', 'JavaScript'],
+      status: 'CONFIRMED',
+      busyStatus: 'BUSY',
+      organizer: { name: 'Admin', email: 'nobody@example.com' },
+      attendees: [
+        { name: 'Adam Gibbons', email: 'adam@example.com', rsvp: true, partstat: 'ACCEPTED', role: 'REQ-PARTICIPANT' },
+        { name: 'Bertrand Delacretaz', email: 'bertrand@example.org', dir: 'https://www.linkedin.com/in/bdelacretaz/', role: 'OPT-PARTICIPANT' }
+      ]
+    });
+  }
+  return events;
+}
+
+function main(_params) {
+  return new Promise(function (resolve, reject) {
+    ics.createEvents(getEvents(), (error, calendar) => {
+      if (error) {
+        reject(error);
+      }
+      resolve({
+        body: calendar,
+        "statusCode": 200,
+        "headers": {
+          "Content-Type": "text/calendar"
+        }
+      })
+    });
+  });
+}
+
+module.exports.main = main
